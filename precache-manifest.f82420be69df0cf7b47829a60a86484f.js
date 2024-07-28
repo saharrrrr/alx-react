c61ddab3e19958a4601c62b0aee7febc78a7912c
saharrrrr <skmao23@gmail.com>
2024-07-28 09:00:06 +0300

    Updates

---
+++ precache-manifest.f82420be69df0cf7b47829a60a86484f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf34b7f142d02371c6f02adf849a65c0",
+    "url": "/alx-react/index.html"
+  },
+  {
+    "revision": "d52dae468fb5209a6260",
+    "url": "/alx-react/static/css/main.2ea72533.chunk.css"
+  },
+  {
+    "revision": "637d78d3447c9cb473f6",
+    "url": "/alx-react/static/js/2.9c671b7c.chunk.js"
+  },
+  {
+    "revision": "7373c6e4eb6af1a9d439",
+    "url": "/alx-react/static/js/3.ba8b7f13.chunk.js"
+  },
+  {
+    "revision": "d52dae468fb5209a6260",
+    "url": "/alx-react/static/js/main.f8c55459.chunk.js"
+  },
+  {
+    "revision": "8f634d997f9161bfdadf",
+    "url": "/alx-react/static/js/runtime~main.7d533d14.js"
+  },
+  {
+    "revision": "292deca118c103b2b28e1c96c1363453",
+    "url": "/alx-react/static/media/holberton-logo.292deca1.jpg"
+  }
+]);
